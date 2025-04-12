0fccfef454a41081ea2439ecb0f5b5dc89d297fb
RewanM0stafa <187568401+RewanM0stafa@users.noreply.github.com>
2025-04-12 08:35:31 +0200

    Add app colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class Assets{
+  static const onBoarding1 = 'assets/videos_gifs/Dementia.gif';
+  static const onBoarding2 = 'assets/videos_gifs/HospitalWheelchair.gif';
+  static const MedicalPrescription  = 'assets/videos_gifs/MedicalPrescription.gif';
+  static const ForgotPassword = 'assets/videos_gifs/ForgotPassword.gif';
+
+
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+abstract class AppColor{
+  static Color primary = const Color(0xff3D55CC);
+  static Color secondary = const Color(0xff5e6aa8);
+  static Color white = const Color(0xffffffff);
+  static Color black = const Color(0xff000000);
+
+  static Color grayBackground = const Color(0xFFF5F5F5); // خلفية الشاشة العامة (خلف البطاقة)
+  static Color grayCard = const Color(0xFFFAFAFA); // خلفية البطاقة (الكونتينر الأبيض)
+  static Color grayInputBorder = const Color(0xFFD9D9D9); // حدود حقول الإدخال
+  static Color grayTextLight = const Color(0xFFA3A3A3); // النصوص التوضيحية أسفل الحقول
+  static Color grayButtonBg = const Color(0xFFE0E0E0); // خلفية زر "Cancel"
+  static Color grayButtonText = const Color(0xFF4B4B4B); // لون النص داخل زر "Cancel"
+
+  /*
+  static Color greyButton = const Color(0xffdbdbdb);
+  static Color lightGray = const Color(0xfff5f6fa);
+  static Color grey3 = const Color(0xffF3F3F3);
+  static Color grey4 = const Color(0xffB3B3B3);
+
+   */
+
+
+
+
+
+}
